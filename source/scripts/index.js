d42ab099e19aaa501058a349e6e2f8ce36d2c766
Alexandr Kolesnikov <89801214+AlexKolbe@users.noreply.github.com>
2025-07-13 21:24:48 +0300

    Module5-task1 (#10)
    * исправление косяков
    
    * продолжаем разметку каталога и карточек
    А также размечаем additional
    
    * исправление ошибок
    
    * сделали котиков
    
    * Закончили разметку Главной полностью
    
    * разметили мобильную версию каталога
    
    * исправление ошибок каталог
    
    * исправление ошибок
    
    * исправление ошибок
    
    * добавили и изменили часть картинок
    
    * стрелочки
    
    * исправление ошибок
    
    * исправили фон на десктопе
    
    * исправление ошибок
    
    * исправление ошибок
    
    * исправление ошибок
    
    * исправление ошибок
    
    * исправление ошибок
    
    * исправление ошибок
    
    * исправление ошибок
    
    * картинки каталога привели к единому размеру
    
    * исправление ошибок
    
    * закончили работу над ошибками
    
    * исправление ошибок

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,16 @@
 /* в этот файл добавляет скрипты*/
+
+const mainNavigation = document.querySelector('.main-nav');
+const mainNavigationToggle = document.querySelector('.main-nav__toggle');
+
+function setMenuState() {
+  mainNavigationToggle.classList.toggle('main-nav__toggle--closed');
+  mainNavigationToggle.classList.toggle('main-nav__toggle--opened');
+}
+
+mainNavigation.classList.remove('main-nav--no-js');
+setMenuState();
+
+mainNavigationToggle.addEventListener('click', () => {
+  setMenuState();
+});
